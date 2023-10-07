b4eab6813715e67de5fa458a06b1224cb80d9f8e
Hassan97-js <hassan.sweden.dev@gmail.com>
2023-10-07 18:31:31 +0200

    update url

--- src/utils/api-request.js
+++ src/utils/api-request.js
@@ -10,7 +10,7 @@ export const makeApiRequest = ({
 }) => {
   return axios({
     method,
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://fiverr-server-2dn7.onrender.com/api",
     url,
     params,
     data,
@@ -20,3 +20,4 @@ export const makeApiRequest = ({
 };
 
 // https://fiverr-server-2dn7.onrender.com/api
+// http://localhost:5000/api
