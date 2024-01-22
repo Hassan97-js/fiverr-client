e08ab12fce944edb6f152fd92019be848de1fff5
Hassan97-js <hassan.sweden.dev@gmail.com>
2024-01-22 10:40:38 +0100

    update api-request.ts

--- src/utils/api-request.ts
+++ src/utils/api-request.ts
@@ -7,7 +7,7 @@ export const makeApiRequest = ({
   url,
   data,
   params,
-  baseURL = "https://fiverr-server-2dn7.onrender.com/api",
+  baseURL = "https://fiverr-clone-app.azurewebsites.net/api",
   headers,
   validateStatus = (status) => {
     return status >= 200 && status < 300;
